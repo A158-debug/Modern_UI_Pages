c6d2f8c7e62036a7fc75f18a9eb7cda2065b3fd2
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2023-03-07 23:29:07 +0530

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Signup from './components/Signup'
+import SignIn from './components/SignIn'
+import Navbar from './components/Navbar'
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Signup/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/signup" element={<Navigate to="/"/>} />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
